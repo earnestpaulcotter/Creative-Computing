bf431351de739fcbea430e8ee2471c6bf07e0ee2
earnestpaulcotter <earnestcotter@gmail.com>
2026-01-15 18:28:04 +0400

    uh oh

---
+++ Class Exercises/111125.py
@@ -0,0 +1,46 @@
+#num = float(input("Enter a Number: "))
+#if num > 0:
+#    print(num,' is a positve number')
+#elif num == 0:
+#    print(num,' is Zero')
+#else: 
+#    print(num,' is a Negative number')
+
+#num = int(input("Display multiplication table of"))
+#for i in range (1, 11):
+#    print(num, 'x', i, '=', num*i)
+
+#counter = 0
+#my_string = 'skibidi toilet'
+#my_char = 'i'
+#for i in my_string:
+#    if i == my_char:
+#        counter += 1
+#print(counter)
+
+#tuple1 = ('Lily', 'Rose', 'Jaismine')
+#tuple2 = ("Peas", "Onion", 'Cabbage')
+
+#print('The 1st element of tuple1 is',tuple1[0])
+#print('The 2nd element of tuple1 is',tuple1[1])
+#print('The 3rd element of tuple1 is',tuple1[2])
+#print('The 1st element of tuple2 is',tuple2[0])
+#print('The 2nd element of tuple2 is',tuple2[1])
+#print('The 2nd element of tuple2 is',tuple2[2])
+
+#print("The element of tuple1 is ",tuple1[0:3])
+#print("The element of tuple2 is ",tuple2[0:3])
+
+#print("tuple1 elements are: ")
+#for i in tuple1:
+#    print(i,end=' ')
+#print('tuple2 elements are: ')
+#for i in tuple2:
+#    print(i,end=' ')
+
+#def myfunc(n):
+#    return lambda a : a * n
+#mydoubler = myfunc(2)
+#a = int(input("Enter the number to be doubled: "))
+#   print("The doubled value is, ", mydoubler(a))
+
---
+++ Class Exercises/111825.py
@@ -0,0 +1,21 @@
+#def my_func():
+#    print("This is my function")
+#my_func()
+
+#def my_func():
+#    print("f")
+#my_func()
+#my_func()
+#my_func()
+
+#def my_func(fname):
+#    print(fname + "Refsnes")
+#my_func("Email")
+#my_func("Tobias")
+#my_func("Linus")
+
+#def my_func(fname, lname):
+#    print(fname, " ", lname)
+
+def myfunc(country = "norway"):
+    print("I am from", country)
